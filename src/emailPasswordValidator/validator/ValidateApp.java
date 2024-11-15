99ddb3ee45c9a533e3ef92437e2fafea1bf636a4
Anastasiia Yemets <anastasiaemec40@gmail.com>
2024-11-15 20:22:18 +0100

    qqq

---
+++ src/emailPasswordValidator/validator/ValidateApp.java
@@ -0,0 +1,40 @@
+package emailPasswordValidator.validator;
+
+public class ValidateApp {
+    public static void main(String[] args) {
+
+        // получаем email от пользователя (сканером, из другого слоя приложения и тд)
+        String email = "estemail@.com";
+
+        String password = "jhjl!1ррррр";
+
+
+        //Валидация email
+        try {
+            EmailAndPasswordValidator.isValidEmail(email);
+            // если мы дойдем до этой строчки кода, значит email валидный
+            // можем создавать нового пользователя
+            System.out.println("The email has passed validation.");
+
+            // Если email прошел проверку - ПРОВЕРЯЕМ ПАРОЛЬ (если email не прошел, то пароль, возможно, проверять не нужно)
+            EmailAndPasswordValidator.isValidPassword(password);
+            System.out.println("Password has passed validation.");
+
+            // Можем создать пользователя, с проверенным email и паролем
+            // User user = new User(email, password)
+            //service.createUser(email, password);
+
+            // service.createUser
+            //  User user = new User(email, password);
+        } catch (EmailValidateException e) {
+            System.out.println("Please provide a valid email address.");
+            String message = e.getMessage();
+            System.out.println(message);
+
+        } catch (PasswordValidateException ex) {
+            // Пароль не прошел проверку (возник объект исключения
+            System.out.println("The password is invalid. Please check the requirements.");
+            System.out.println(ex.getMessage());
+        }
+    }
+}
