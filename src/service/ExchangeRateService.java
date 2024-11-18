facd029bb153fe6bec699d47f3517754986c0eae
Anastasiia Yemets <anastasiaemec40@gmail.com>
2024-11-18 09:01:43 +0100

    new in ExchangeService

--- src/service/ExchangeRateService.java
+++ src/service/ExchangeRateService.java
@@ -2,6 +2,9 @@ package service;
 
 public interface ExchangeRateService {
 
+    // Метод для инициализации курсов валют относительно евро
+    void initializeExchangeRates();
+
     double getExchangeRate(String fromCurrencyCode, String toCurrencyCode);
 
     // Обновить курс валюты (доступно только админу)
