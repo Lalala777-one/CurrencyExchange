a00103b0763f7f7d5e73915deab60e93808a7882
Tanya Maxar <kolt.levry@gmail.com>
2024-11-18 11:45:39 +0100

    update Repo

--- src/view/AppCurrencyExchange.java
+++ src/view/AppCurrencyExchange.java
@@ -9,7 +9,6 @@ import java.util.List;
 public class AppCurrencyExchange {
     public static void main(String[] args) {
 
-
         AccountRepo accountRepo = new AccountRepoImpl();
         ExchangeRateRepo exchangeRateRepo = new ExchangeRateRepoImpl();
         TransactionRepo transactionRepo = new TransactionRepoImpl();
