8b8292ab67dca1e1a7bd958b524d2becba8dad61
Tanya Maxar <kolt.levry@gmail.com>
2024-11-20 11:49:52 +0100

    updates currencyServiceImpl

--- src/service/CurrencyServiceImpl.java
+++ src/service/CurrencyServiceImpl.java
@@ -2,19 +2,36 @@ package service;
 
 import model.Currency;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CurrencyServiceImpl implements CurrencyService{
 
+    private final Map<String, Currency> currencyMap = new HashMap<>();
 
     @Override
     public void addCurrency(Currency currency) {
-
+        if (currency == null || currency.getCode() == null || currency.getCode().isBlank()) {
+            throw new IllegalArgumentException("Валюта или ее код не могут быть пустыми.");
+        }
+        if (currencyMap.containsKey(currency.getCode())) {
+            throw new IllegalArgumentException("Валюта с кодом " + currency.getCode() + " уже существует.");
+        }
+        currencyMap.put(currency.getCode(), currency);
+        System.out.println("Валюта " + currency.getName() + " додана успішно.");
     }
 
     @Override
     public Currency getCurrencyByCode(String currencyCode) {
-        return null;
+        if (currencyCode == null || currencyCode.isBlank()) {
+            throw new IllegalArgumentException("Код валюты не может быть пустым.");
+        }
+        Currency currency = currencyMap.get(currencyCode);
+        if (currency == null) {
+            throw new IllegalArgumentException("Валюта с кодом " + currencyCode + " найдена не найдена.");
+        }
+        return currency;
     }
 
     @Override
